34f2fa28e6c6c4d0fb7af01865d0fe916cf6f135
Nikhil Saxena <nikhilsaxena835@gmail.com>
2026-05-14 00:27:14 +0530

    bug fix in history files

--- history.cpp
+++ history.cpp
@@ -8,14 +8,41 @@
 #include <ostream>
 #include <queue>
 #include <string>
+#include <cctype>
+#include <cstdlib>
 
 using namespace std;
 const int maxSize = 20;
 string filePath;
+static vector<string> *history_ptr = nullptr;
+static bool history_dirty = false;
+static bool atexit_registered = false;
+static int history_pending = 0;
+static const int history_flush_interval = 10;
+
+static bool isWhitespaceOnly(const string &s) {
+    for (char c : s) {
+        if (!isspace(static_cast<unsigned char>(c))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static void flush_history_on_exit() {
+    if (history_ptr != nullptr && history_dirty) {
+        save_history(*history_ptr);
+    }
+}
 
 void history_initiate(vector<string> &q, string homedir) {
 
     filePath = homedir + "/history.txt";
+    history_ptr = &q;
+    if (!atexit_registered) {
+        atexit(flush_history_on_exit);
+        atexit_registered = true;
+    }
     ifstream file(filePath);
     string line;
 
@@ -32,12 +59,27 @@ void history_initiate(vector<string> &q, string homedir) {
 
 
 void add_history(vector<string> &q, char * command) {
-    if(q.back() == command) {return;}
+    if (command == nullptr) {
+        return;
+    }
+    string cmd = command;
+    if (isWhitespaceOnly(cmd)) {
+        return;
+    }
+    if (!q.empty() && q.back() == cmd) {
+        return;
+    }
     if (q.size() == maxSize) {
         q.erase(q.begin());
     }
-    q.push_back(command);
-    save_history(q);
+    q.push_back(cmd);
+    history_dirty = true;
+    history_pending++;
+    if (history_pending >= history_flush_interval) {
+        save_history(q);
+        history_dirty = false;
+        history_pending = 0;
+    }
 }
 
 void print_history(vector<string> &q, int n) {
